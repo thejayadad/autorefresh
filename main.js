bf57a69329c6df8f99432fdc4affef52a6dad4b3
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-10-17 18:06:00 -0400

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+// let count = 6;
+// let counter = document.getElementById("counter");
+
+// setInterval(() => {
+//     document.getElementById("counter").innerText = count;
+//     count--
+//     if(count < 1 ) location.reload()
+// }, 1000)
